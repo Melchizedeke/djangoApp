018578a6c299ac8cd099b9c62b0d10ff41e74a1e
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-02-04 15:11:00 +0100

    Modify the views.py and the urls.py files

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Hello, world. Welcome to the Home Page.</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>Welcome to the About Page.</h1>")

e9fb1e832fd5d461c507d75cd76b934da1656f19
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-02-05 17:01:30 +0100

    modify code

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author':'John Igoche',
+        'title':'Hello World Post',
+        'content':'I am happy to make my first ever post on here, cheers!',
+        'date_posted':'5th February 2025.'
+    },
+    {   'author': 'Daniel Ekpo',
+        'title': 'My First Post',
+        'content': 'Wow, I am happy to also make my first ever post on here, cheers!',
+        'date_posted': '4th February 2025.'}
+]
 
 def home(request):
-    return HttpResponse("<h1>Hello, world. Welcome to the Home Page.</h1>")
+    context = {
+        'data':posts
+    }
+    return render(request, "blog/home.html", context=context)
 
 
 def about(request):
-    return HttpResponse("<h1>Welcome to the About Page.</h1>")
+    return render(request, "blog/about.html", {'title':'About'})

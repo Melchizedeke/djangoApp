2917bb15a53f30f8c61d7e405dbe2a1001d139c0
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-02-09 22:50:55 +0100

    Modify the blog views.py file

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author':'John Igoche',
-        'title':'Hello World Post',
-        'content':'I am happy to make my first ever post on here, cheers!',
-        'date_posted':'5th February 2025.'
-    },
-    {   'author': 'Daniel Ekpo',
-        'title': 'My First Post',
-        'content': 'Wow, I am happy to also make my first ever post on here, cheers!',
-        'date_posted': '4th February 2025.'}
-]
 
 def home(request):
     context = {
-        'data':posts
+        'posts': Post.objects.all()
     }
-    return render(request, "blog/home.html", context=context)
+
+    # context = {
+    #     'posts': posts
+    # }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):

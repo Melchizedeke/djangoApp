517b9b902ade055c90715a5e3530257d5ed727e1
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-03-06 18:02:38 +0100

    Change to blog urls.py

--- myBlog/myBlog/urls.py
+++ myBlog/myBlog/urls.py
@@ -26,6 +26,18 @@ urlpatterns = [
     path('register/', include("users.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+         name='password_reset'),
+    path('password-reset/done', 
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('password-reset-complete', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
+         name='password_reset_complete'),
     path('', include("blog.urls")),
     path('profile/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
@@ -34,19 +46,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-
-
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.contrib.auth import views as auth_views
-# from users import views
-
-# urlpatterns = [
-#     path('register/', include("users.urls")),
-#     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-#     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-#     path('', include("blog.urls")),
-#     path('profile/', views.profile, name='profile'),
-#     path('admin/', admin.site.urls),  # Move this to the end
-# ]
 

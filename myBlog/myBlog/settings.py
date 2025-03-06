22033957bf6421c92785da8fb3861cbed9c2c84e
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2025-03-06 18:01:46 +0100

    Change settings

--- myBlog/myBlog/settings.py
+++ myBlog/myBlog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +144,13 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGOUT_REDIRECT_URL = 'login'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+EMAIL_HOST_USER = 'igochej4@gmail.com'
+EMAIL_HOST_PASSWORD = 'uinx fksn jfcr wpgk'
